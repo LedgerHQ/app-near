ea27f91111af7f483e13d4edbdef72f46cefe2ef
dj8yf0μl <feepdake27@gmail.com>
2024-02-09 22:46:20 +0200

    test: `test_get_public_key_and_confirm_screen` added

---
+++ tests/common.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+ROOT_SCREENSHOT_PATH = Path(__file__).parent.resolve()
+# m/44'/397'/0'/0'/1
+DERIV_PATH_DATA = bytes.fromhex("8000002c8000018d800000008000000080000001")
+
+DEFAULT_KEY = bytes.fromhex("c4f5941e81e071c2fd1dae2e71fd3d859d462484391d9a90bf219211dcbb320f")
