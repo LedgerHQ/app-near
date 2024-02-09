4fc3c877754add5763a167cc14dfadb95315b0fe
dj8yf0μl <feepdake27@gmail.com>
2024-02-09 21:56:58 +0200

    test: add `test_get_version_cmd`

---
+++ tests/test_version_cmd.py
@@ -0,0 +1,11 @@
+from utils import Nearbackend
+
+# In this test we check that the get_version replies the right application version
+def test_get_version_cmd(backend):
+    # Use the app interface instead of raw interface
+    client = Nearbackend(backend)
+    # Send the get_version instruction to the app
+    version = client.get_version().data
+    assert len(version) == 3
+    # Assert that we have received the correct app version compared as Makefile data
+    assert (version[0], version[1], version[2]) == (1, 0, 0)
---
+++ tests/utils.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from ragger.backend.interface import RAPDU, BackendInterface
+
+CLA = 0x80
+INS_GET_APP_CONFIGURATION = 0x06
+
+# Parameter not used for this APDU
+P1_P2_NOT_USED = 0x57
+
+@dataclass
+class Nearbackend():
+    backend: BackendInterface
+
+    def get_version(self) -> RAPDU:
+        return self.backend.exchange(CLA, INS_GET_APP_CONFIGURATION, P1_P2_NOT_USED, P1_P2_NOT_USED, bytes())
