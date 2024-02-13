b013cbf7731d8a2c5bcedc41cdaccbda11269f0e
dj8yf0μl <feepdake27@gmail.com>
2024-02-13 23:24:20 +0200

    test: add `test_sign_delete_account_long`

--- tests/test_sign_transaction/test_delete_account.py
+++ tests/test_sign_transaction/test_delete_account.py
@@ -50,3 +50,58 @@ def test_sign_delete_account_short(firmware, backend, navigator: Navigator, test
         )
     ]
     generic_test_sign(client, chunks, navigator, test_name)
+
+def test_sign_delete_account_long(firmware, backend, navigator: Navigator, test_name):
+    """
+    Transaction {
+        signer_id: AccountId(
+            "c4f5941e81e071c2fd1dae2e71fd3d859d462484391d9a90bf219211dcbb320f",
+        ),
+        public_key: ed25519:EFr6nRvgKKeteKoEH7hudt8UHYiu94Liq2yMM7x2AU9U,
+        nonce: 103595482000005,
+        receiver_id: AccountId(
+            "dc7e34eecec3096a4a661e10932834f801149c49dba9b93322f6d9de18047f9c",
+        ),
+        block_hash: Cb3vKNiF3MUuVoqfjuEFCgSNPT79pbuVfXXd2RxDXc5E,
+        actions: [
+            DeleteAccount(
+                DeleteAccountAction {
+                    beneficiary_id: AccountId(
+                        "dc7e34eecec3096a4a661e10932834f801149c49dba9b93322f6d9de18047f9c1b11b3b31673033936ad07bddc01f9da27d974811e480fb197c799e23480a489",
+                    ),
+                },
+            ),
+        ],
+    }   
+    """
+    client = Nearbackend(backend)
+    chunks = [
+        AsyncAPDU(
+            data=bytes.fromhex(
+                "80020057fa8000002c8000018d800000008000000080000001400000006334663539343165383165303731633266643164616532653731666433643835396434363234383433393164396139306266323139323131646362623332306600c4f5941e81e071c2fd1dae2e71fd3d859d462484391d9a90bf219211dcbb320f85aae733385e00004000000064633765333465656365633330393661346136363165313039333238333466383031313439633439646261396239333332326636643964653138303437663963ac299ac1376e375cd39338d8b29225613ef947424b74a3207c1226863a725831010000000780000000646337653334656563656333"
+            ),
+            navigable_conditions=NavigableConditions(
+                value=["Continue to actions"],
+            ),
+            expected_response=RAPDU(
+                SW_OK,
+                bytes(),
+            ),
+        ),
+        AsyncAPDU(
+            data=bytes.fromhex(
+                "80028057743039366134613636316531303933323833346638303131343963343964626139623933333232663664396465313830343766396331623131623362333136373330333339333661643037626464633031663964613237643937343831316534383066623139376337393965323334383061343839"
+            ),
+            navigable_conditions=NavigableConditions(
+                value=["Sign"],
+            ),
+            expected_response=RAPDU(
+                SW_OK,
+                # signature
+                bytes.fromhex(
+                    "318ab694a182f1793d7796f8741fa399c44ef033fcebef23a57293a37f536e53a993ecd7b480763dc154d606d39bdae8f90f698a935e856e8c0114a6d9567009"
+                ),
+            ),
+        )
+    ]
+    generic_test_sign(client, chunks, navigator, test_name)
