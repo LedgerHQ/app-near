e980880d7765969cac8c0830f275b568d8934122
GroM <jerome.caporossi@ledger.fr>
2025-01-14 15:28:21 +0100

    Update after PR review

--- tests/test_version_cmd.py
+++ tests/test_version_cmd.py
@@ -1,4 +1,5 @@
 from application_client.client import Nearbackend
+import toml
 
 
 # In this test we check that the get_version replies the right application version
@@ -8,5 +9,9 @@ def test_get_version_cmd(backend):
     # Send the get_version instruction to the app
     version = client.get_version().data
     assert len(version) == 3
-    # Assert that we have received the correct app version compared as Makefile data
-    assert (version[0], version[1], version[2]) == (2, 3, 3)
+    # Read version from Cargo.toml
+    with open('Cargo.toml', 'r') as f:
+        config = toml.load(f)
+        version = config['package']['version']
+        major, minor, patch = version.split('.')
+    assert (version[0], version[1], version[2]) == (int(major), int(minor), int(patch))
